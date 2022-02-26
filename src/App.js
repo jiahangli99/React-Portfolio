280b2d31916529c496efcbdb62f3600ee2aefaa6
jiahangli99 <liabraham.1999@gmail.com>
2022-02-26 12:10:36 -0700

    added about page, need more styles and animations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Main from './Components/Main/main';
 import { Nav } from './Components/Nav/Nav';
-import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Main />
-      <Footer />
     </div>
   );
 }
--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './about.css'
 function about(props) {
     return (
-        <div>
-            <h1>hello world</h1>
+        <div className='about'>
+            <h1>About Me</h1>
+            <p className='aboutp'>I’m a Web Developer located in Denver. I have a serious passion for web development, and creating a great experience for the users.</p>
+            <p className='aboutp'>I am also a problem solver, independent person with a need to consistently learn and improve. Love making music, playin games, and going camping on the side.</p>
         </div>
     );
 }
