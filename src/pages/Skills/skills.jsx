cc792755158ce2bfb52932c580a61d9477487b5b
jiahangli99 <liabraham.1999@gmail.com>
2022-03-08 16:30:14 -0700

    added basic css and content for skills

--- src/pages/Skills/skills.jsx
+++ src/pages/Skills/skills.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './skills.css'
 
 function skills(props) {
     return (
-        <div>
-            <p>COMING SOON</p>
+        <div className='skills'>
+            <h1>My Skills</h1>
+            <p className='skillsp'>I create responsive websites that are easy to use. The skills that I'm best at for front-end development are HTML, CSS, JS and using React.js to make a single page website.
+</p>
         </div>
     );
 }
