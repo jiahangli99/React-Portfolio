b4ef3533bef17530506be9b65b1d41b49de8535a
jiahangli99 <liabraham.1999@gmail.com>
2022-03-08 18:17:52 -0700

    added some message

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -4,6 +4,8 @@ import './contact.css'
 function contact(props) {
     return (
         <div>
+            <h1>Contact Me</h1>
+            <p>I'm always open to meeting new people</p>
             <form action="https://formsubmit.co/liabraham.1999@gmail.com" method="POST">
         <input type="hidden" name="_subject" value="Portfolio"/>
         <input type="text" name="name" placeholder="Name" required/>
