731ba5c8e7f96fc6e3c05b37a9d4002f1b04b971
jiahangli99 <liabraham.1999@gmail.com>
2022-03-10 10:06:05 -0700

    added pictures and template for projects

--- src/pages/Projects/project.jsx
+++ src/pages/Projects/project.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './project.css'
+import LiCode from '../../picture/LiCode.png'
+import Capsule from '../../picture/Project-Capsule.png'
+import Note from '../../picture/Reactive-Notes.png'
 
 function project(props) {
     return (
         <div>
-            <h1>Coming Soon</h1>
+            <div className='projectsContainer'>
+                <div className='project'>
+                    <a href="https://licodewastaken.herokuapp.com/">
+                    <img className='projectPic' src={LiCode} alt="" />
+                    </a>
+                    <p>LiCode</p>
+                </div>
+                <div className='project'>
+                    <a href="https://projectcapsule.netlify.app/">
+                    <img className='projectPic' src={Capsule} alt="" />
+                    </a>
+                    <p>Project Capsule</p>
+                </div>
+                <div className='project'>
+                    <a href="https://reactive-note.netlify.app/">
+                    <img className='projectPic' src={Note} alt="" />
+                    </a>
+                    <p>Reactive Notes</p>
+                </div>
+            </div>
         </div>
     );
 }
