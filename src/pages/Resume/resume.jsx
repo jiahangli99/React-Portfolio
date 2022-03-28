154566895c9cbc92c016347a559181fb80fc5208
jiahangli99 <liabraham.1999@gmail.com>
2022-03-28 12:10:43 -0600

    Added download button for resume

--- src/pages/Resume/resume.jsx
+++ src/pages/Resume/resume.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-
-
-function Resume(props) {
-    return (
-        <div>
-            Coming Soon
-        </div>
-    );
+import React, {Component} from 'react';
+import jsPDF from 'jspdf';
+import ResumePdf from '../../logo/Resume.png'
+class Resume extends Component {
+    pdfGenerate=()=>{
+        let doc=new jsPDF('landscape', 'px', 'b4', 'false');
+        doc.addImage(ResumePdf, 'PNG', 100,10,500,550)
+        doc.save('JiahangLiResume.pdf')
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.pdfGenerate}>Download Resume</button>
+            </div>
+        )
+    }
 }
 
 export default Resume;
